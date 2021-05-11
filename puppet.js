663db1f0632c91c04d17eb5787ad7db8bc7461e3
Rabia Okatan <rabiaro26@gmail.com>
2021-05-11 14:14:58 +0300

    kuklayı sürükleyerek konumlandırma eklendi

--- puppet.js
+++ puppet.js
@@ -1,22 +1,25 @@
 var myGamePiece;
+var xPos=20;
+var yPos=100;
 
 function startGame() {
-    myGamePiece = new component(120, 120, "./pikachu.png", 10, 100, "image");
+    myGamePiece = new component(120, 120, "images/pikachu.png", xPos, yPos, "image");
     myGameArea.start();
+
 }
 
 var myGameArea = {
-    canvas : document.getElementById('canvas'),
-    start : function() {
+    canvas: document.getElementById('canvas'),
+    start: function () {
         this.context = this.canvas.getContext("2d");
-                document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-                this.frameNo = 0;
-                this.interval = setInterval(updateGameArea, 20);
-        },
-    clear : function() {
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    stop : function() {
+    stop: function () {
         clearInterval(this.interval);
     }
 }
@@ -30,25 +33,28 @@ function component(width, height, color, x, y, type) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;    
-    this.update = function() {
+    this.speedY = 0;
+    this.x=xPos;
+    this.y=yPos;
+    this.update = function () {
         ctx = myGameArea.context;
         if (type == "image") {
-            ctx.drawImage(this.image, 
-                this.x, 
+
+            ctx.drawImage(this.image,
+                this.x,
                 this.y,
                 this.width, this.height);
+
         } else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    this.newPos = function() {
+    this.newPos = function () {
         this.x += this.speedX;
-        this.y += this.speedY;        
+        this.y += this.speedY;
     }
+
 }
 
 function updateGameArea() {
@@ -57,25 +63,33 @@ function updateGameArea() {
     myGamePiece.update();
 }
 
+//Direkt mouseun konumunu x ve y'ye atadığı için aşağıdaki fonksiyonlar hatalı çalışıyor.Düzeltilecek.
+//kuklayı sürükleyerek konumlandırmak için
+window.addEventListener('mousedown', function (e) {
+    myGamePiece.x = e.x;
+    myGamePiece.y = e.y;
+});
+
+
 function moveup() {
-    myGamePiece.speedY = -10; 
+    myGamePiece.speedY = -10;
 }
 
 function movedown() {
-    myGamePiece.speedY = 10; 
+    myGamePiece.speedY = 10;
 }
 
 function moveleft() {
-    myGamePiece.speedX = -10; 
+    myGamePiece.speedX = -10;
 }
 
 function moveright() {
-    myGamePiece.speedX = 10; 
+    myGamePiece.speedX = 10;
 }
 
 function clearmove() {
-    myGamePiece.speedX = 0; 
-    myGamePiece.speedY = 0; 
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
 }
 
 
