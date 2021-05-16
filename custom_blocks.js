6ae74813e4576ed7ce7181df86a977c91b1b0859
Rabia Okatan <rabiaro26@gmail.com>
2021-05-16 21:39:09 +0300

    yeni bloklar eklendi

---
+++ custom_blocks.js
@@ -0,0 +1,47 @@
+//sadece ilk oluşturduğum gözüküyor???
+
+'use strict';
+
+Blockly.Blocks['move'] = {
+    init: function () {
+        this.appendValueInput("move")
+            .setCheck("Number")
+            .appendField("move");
+        this.appendDummyInput()
+            .appendField("steps");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(178);
+        this.setTooltip("step number");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.JavaScript['move'] = function (block) {
+    var value_move = Blockly.JavaScript.valueToCode(block, 'move', Blockly.JavaScript.ORDER_ATOMIC);
+    // TODO: Assemble JavaScript into code variable.
+    var code = moveright();
+    return code;
+};
+ 
+ Blockly.Blocks['when_flag_clicked'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("when")
+          .appendField(new Blockly.FieldImage("https://scratch.mit.edu/static/assets/2e0c4790f8f9cf28e3c346b9cef0fcb6.svg", 15, 15, { alt: "*", flipRtl: "FALSE" }))
+          .appendField("clicked");
+      this.setInputsInline(false);
+      this.setNextStatement(true, null);
+      this.setColour(60);
+   this.setTooltip("when the flag clicked it starts the game");
+   this.setHelpUrl("");
+    }
+  };
+
+Blockly.JavaScript['when_flag_clicked'] = function (block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
+};
+
