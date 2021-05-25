99878d5ba42e26a4a07139b0b05e89141c14d164
Rabia Okatan <rabiaro26@gmail.com>
2021-05-25 21:55:26 +0300

    ardublockly eklendi

---
+++ ardublockly/custom_blocks.js
@@ -0,0 +1,76 @@
+Blockly.Blocks['move'] = {
+    init: function() {
+      this.appendValueInput("move")
+          .setCheck("Number")
+          .appendField("move");
+      this.appendDummyInput()
+          .appendField("steps to");
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldDropdown([["right","right"], ["left","left"], ["up","up"], ["down","down"]]), "direction");
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(178);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.JavaScript['move'] = function(block) {
+    var value_move = Blockly.JavaScript.valueToCode(block, 'move', Blockly.JavaScript.ORDER_ATOMIC);
+    var dropdown_direction = block.getFieldValue('direction');
+
+    var method_name = dropdown_direction;
+    var method_prefix = "move_";
+ 
+    var code =window[method_prefix + method_name](value_move);
+    return code;
+  };
+
+  //hatalı çalışıyor
+  Blockly.Blocks['go_to'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("go to x:")
+          .appendField(new Blockly.FieldTextInput(""), "x_pos")
+          .appendField("y:")
+          .appendField(new Blockly.FieldTextInput(""), "y_pos");
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(178);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.JavaScript['go_to'] = function(block) {
+    var text_x_pos = block.getFieldValue('x_pos');
+    var text_y_pos = block.getFieldValue('y_pos');
+    
+    var code = myGamePiece.x=text_x_pos;
+    var code= myGamePiece.y=text_y_pos;
+    return code;
+  };
+
+Blockly.Blocks['when_flag_clicked'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("when")
+            .appendField(new Blockly.FieldImage("images/flag.png", 15, 15, { alt: "*", flipRtl: "FALSE" }))
+            .appendField("clicked");
+        this.setInputsInline(false);
+        this.setNextStatement(true, null);
+        this.setColour(60);
+        this.setTooltip("when the flag clicked it starts the game");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.JavaScript['when_flag_clicked'] = function (block) {
+  
+    var code = "...\n";
+    return code;
+};
+
+
---
+++ ardublockly/puppet.js
@@ -0,0 +1,109 @@
+var myGamePiece;
+var step = 0;
+var imgObj = new Image();
+
+
+function startGame() {
+    myGamePiece = new component(120, 120, "images/pikachu.png", 20, 120, "image");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas: document.getElementById('canvas'),
+    start: function () {
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function (img_path) {
+       
+        ctx = myGameArea.context;
+
+        //arka plan resmi icin ancak guncellenmiyor
+        if (img_path === undefined) img_path = 'images/backdrop/mat-01.PNG';
+        ctx.drawImage(imgObj, 0, 0);
+        imgObj.src = img_path;
+        //console.log(img_path);
+
+        if (type == "image") {
+
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width, this.height);
+
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+}
+
+function updateGameArea(img_path) {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update(img_path);
+
+}
+
+//kuklayi surukleyerek konumlandirmak icin
+window.addEventListener('mousedown', function (e) {
+
+    if (e.x < myGameArea.canvas.width && e.y < myGameArea.canvas.height) {
+        myGamePiece.x = e.x;
+        myGamePiece.y = e.y;
+
+        return
+    }
+});
+
+function move_up(step) {
+    myGamePiece.speedY = -1 * step;
+}
+
+function move_down(step) {
+    myGamePiece.speedY = 1 * step;
+}
+
+function move_left(step) {
+    myGamePiece.speedX = -1.75 * step;
+}
+
+function move_right(step) {
+    myGamePiece.speedX = step * 1.75;
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+}
+
+
+
+
